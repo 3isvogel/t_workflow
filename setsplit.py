b84abd18063842bdf1f1ed10c03651d659687c11
3isvogel <3isvogel>
2024-11-20 15:59:02 +0900

    Minor changes to the API

--- setsplit.py
+++ setsplit.py
@@ -1,5 +1,13 @@
 import numpy as np
-def _get_numerosity(labels):
+def get_numerosity(labels: np.ndarray) -> np.ndarray:
+    """Get numerosity from a one-hot encoded numpy array with two dimensions (including one-hot)
+
+    Args:
+        labels (np.ndarray): labels to count
+
+    Returns:
+        np.ndarray: occurrencies of elements, element coded with 1 in position k is in the k'th position of the array
+    """
     # using axis as below, should account for any shape, assuming labels occurs in last position,
     # once per data point
     axis=tuple(i for i in range(labels.ndim - 1))
@@ -7,7 +15,7 @@ def _get_numerosity(labels):
 
 # Compute class weight
 def _get_class_weight(labels):
-    class_num = _get_numerosity(labels)
+    class_num = get_numerosity(labels)
     print(f'Count: {class_num}')
     total_samples = np.sum(class_num)
     # Previous weight was wrong
@@ -38,7 +46,7 @@ def _to_remove(array, occurrencies):
 
 def _downsample(sets):
     # TODO: shuffle the set somewhere
-    y_cnt = _get_numerosity(sets[-1])
+    y_cnt = get_numerosity(sets[-1])
     y_diff = y_cnt - np.min(y_cnt)  # remove the minimum from all of them, obtaining the exact number of values to eremove
     remove_mask = _to_remove(sets[-1], y_diff)
     keep_mask = 1 - remove_mask
@@ -48,15 +56,16 @@ def _downsample(sets):
 
     return keep_set, remove_set
 
-def prepare_sets(*data, labels, splits: tuple[float], downsample: bool = False) -> tuple[list[np.ndarray], list[np.ndarray], dict[float]]:
+def prepare_sets(*data, labels: np.ndarray, splits: tuple[float], downsample: bool = False) -> tuple[list[np.ndarray], list[np.ndarray], dict[float]]:
     """Prepare sets for training, splitting the setsaccording to the values in `split`
 
     Args:
+        labels (np.ndarray): one-hot encoded numpy array, must be bidimensional (including the one-hot encoding)
         splits (tuple[float]): values between 0 and 1, these are used as percentages to split the data, note that the process is iterative: the first value is the percentage of the data, the second is the value of the remaining data, and so on
         downsample (bool, optional): balance data downsampling it. Defaults to False.
 
     Returns:
-        tuple[list[np.ndarray], list[np.ndarray], dict[float]]: the first element is the actual split, remainder is the remaining data from downsampling, `class_weight` is the inverse of numersoity, as required by `Model.fit` for reducing overfit, according to the value of `downsample` either `remainder` or `class_weight` is None
+        tuple[list[np.ndarray], list[np.ndarray], dict[float]]: `sets, remainder, class_weight` the first element is the actual split, remainder is the remaining data from downsampling, `class_weight` is the inverse of numersoity, as required by `Model.fit` for reducing overfit, according to the value of `downsample` either `remainder` or `class_weight` is None
     """
     class_weight = None
     remainder = None
